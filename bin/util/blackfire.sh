b3f360d8f33c1c8d493a6f659ada0229c0ca7f0e
David Zuelke <dzuelke@salesforce.com>
2025-07-17 14:37:34 +0200

    NEEDS GUS: Unify indentation of platform install sub-steps
    Each userland package providing something native is now a "bullet", and each provided native package that gets force-installed is a sub-bullet (or gets a similarly indented notice).
    
    Same for APM auto-installs at the end of the build: the detection of either Blackfire or New Relic is a "bullet", and the package installs are a sub-bullet (or a notice if no suitable ext is available).
    
    Looks as follows:
    
        -----> Installing platform packages...
               - php (8.3.23)
               - apache (2.4.63)
               - composer (2.8.9)
               - nginx (1.28.0)
               NOTICE: detected userland polyfill packages for PHP extensions
               NOTICE: now attempting to install native extension packages
               - Installing extensions provided by symfony/polyfill-uuid:
                 - ext-uuid (1.3.0)
               - Installing extensions provided by dummypak/ext-pq-polyfill:
                 - ext-raphf (2.0.1)
                 - ext-pq (2.2.3)
               - Installing extensions provided by symfony/polyfill-ctype:
                 - ext-ctype (already enabled)
               - Installing extensions provided by symfony/polyfill-mbstring:
                 - ext-mbstring (bundled with php)
        -----> Installing dependencies...
               Composer version 2.8.9 2025-05-13 14:01:37
               Installing dependencies from lock file
               Verifying lock file contents can be installed on current platform.
               Package operations: 63 installs, 0 updates, 0 removals
                 - Installing php-http/discovery (1.20.0): Extracting archive
                 - Installing symfony/polyfill-uuid (v1.32.0): Extracting archive
               Generating optimized autoload files
               30 packages you are using are looking for funding.
               Use the `composer fund` command to find out more!
        -----> Preparing runtime environment...
        -----> Checking for additional extensions to install...
               - Blackfire config vars detected, installing ext-blackfire...
                 - blackfire (2.28.28)
                 - ext-blackfire (1.92.40)
               - New Relic config var detected, installing ext-newrelic..
        	       NOTICE: no suitable extension available

--- bin/util/blackfire.sh
+++ bin/util/blackfire.sh
@@ -6,10 +6,10 @@ install_blackfire_ext() {
 	BLACKFIRE_SERVER_ID=${BLACKFIRE_SERVER_ID:-}
 	BLACKFIRE_SERVER_TOKEN=${BLACKFIRE_SERVER_TOKEN:-}
 	if [[ -n "$BLACKFIRE_SERVER_TOKEN" && -n "$BLACKFIRE_SERVER_ID" ]] && ! platform-composer show --installed --quiet heroku-sys/ext-blackfire 2>/dev/null; then
-		notice_inline "Blackfire config vars detected, installing ext-blackfire..."
-		if ! platform-composer require --update-no-dev -- "heroku-sys/ext-blackfire:*" "heroku-sys/ext-blackfire.native:*" >> $build_dir/.heroku/php/install.log 2>&1; then
+		echo "- Blackfire config vars detected, installing ext-blackfire..." | indent
+		if ! PHP_PLATFORM_INSTALLER_DISPLAY_OUTPUT_INDENT=9 platform-composer require --update-no-dev -- "heroku-sys/ext-blackfire:*" "heroku-sys/ext-blackfire.native:*" >> $build_dir/.heroku/php/install.log 2>&1; then
 			mcount "warnings.addons.blackfire.extension_missing"
-			warning_inline "no suitable version of ext-blackfire available"
+			warning_inline -i9 "no suitable version of ext-blackfire available"
 		fi
 	fi
 }
